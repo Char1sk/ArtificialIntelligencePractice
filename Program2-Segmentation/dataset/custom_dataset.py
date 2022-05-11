9bfcebe6084ae4a732c0ffed3f29a8411ee5681a
Charisk <2553036255@qq.com>
2022-05-11 22:59:05 +0800

    trainDeepLab, custom_dataset,

---
+++ Program2-Segmentation/dataset/custom_dataset.py
@@ -0,0 +1,89 @@
+################ CHANGED ##################
+# Move test into main function
+# Move loadimage loadmask into class
+# Make transform be a parameter
+# Make transform at 320x240
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from PIL import Image
+import os
+import numpy as np
+
+class MyDataset(Dataset):
+    
+    def __init__(self, path, isTrain, transform):
+        self.path = path
+        self.isTrain = isTrain
+        self.transform = transform
+        
+        self.horizonPath = 'horizons.txt'
+        self.imagesPath = 'images'
+        self.labelsPath = 'labels'
+        
+        self.names = []
+        with open(os.path.join(self.path, self.horizonPath)) as f:
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                m = [i for i in line.split(' ')]
+                isStandard = int(m[1]) == 320 and int(m[2]) == 240
+                if (self.isTrain and isStandard) or \
+                        (not self.isTrain and not isStandard):
+                    self.names.append(m)
+        
+    def __getitem__(self, index):
+        fn = self.names[index][0]
+        image = self.loadimage(fn)
+        mask = self.loadmask(fn)
+        # return {'img': image, 'mask': mask,}
+        # print(image.shape, mask.shape)
+        return (image, mask)
+    
+    def __len__(self):
+        return len(self.names)
+    
+    def loadimage(self, path):
+        img_pil = Image.open(os.path.join(self.path, self.imagesPath, f'{path}.jpg'))
+        img_pil = self.transform(img_pil)
+        return img_pil
+    
+    def loadmask(self, path):
+        mask=[]
+        with open(os.path.join(self.path, self.labelsPath, f'{path}.regions.txt')) as f:
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                m = [int(i) if len(i)==1 else 8 for i in line.split(' ')]
+                mask.append(m)
+        mask = Image.fromarray(np.array(mask))
+        mask = self.transform(mask)
+        return mask
+    
+
+if __name__ == '__main__':
+    
+    transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
+    
+    fpath = './iccv09Data'
+    
+    train_set = MyDataset(fpath, True, transform)
+    train_loader = DataLoader(train_set, batch_size=1, shuffle=True)
+
+    test_set = MyDataset(fpath, False, transform)
+    test_loader = DataLoader(test_set, batch_size=1, shuffle=True)
+
+    count = 0
+    for ii, sample in enumerate(train_loader):
+        count = count+1
+    print(count)
+    
+    count = 0
+    for ii, sample in enumerate(test_loader):
+        count = count+1
+    print(count)
---
+++ Program2-Segmentation/trainDeepLab.py
@@ -0,0 +1,126 @@
+import torch
+import torch.nn as nn
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import time
+import numpy as np
+
+from modeling.deeplab import DeepLab
+from dataset.custom_dataset import MyDataset
+
+
+def calMIOU(pred, label):
+    nc = pred.shape[1]
+    _, index = pred.max(dim=1)
+    # print(index.shape, label.shape)
+    index, label = index.cpu().numpy(), label.cpu().numpy()
+    miou = 0.0
+    for i, l in zip(index, label):
+        # print(i.shape, l.shape)
+        hist = np.zeros((nc, nc), dtype=int)
+        for ri, rl in zip(i, l):
+            # print(nc*rl.astype(int)+ri)
+            hist += np.bincount(nc*rl.astype(int)+ri, minlength=nc**2).reshape(nc, nc)
+        # print('hist:', hist)
+        # print(np.diag(hist))
+        # print((hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist)))
+        miou += (np.diag(hist) / (hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist) + 1) ).mean()
+        # print('miou:', miou)
+    return miou
+
+
+def loadData():
+    datapath = './iccv09Data'
+    data_transforms = transforms.Compose([
+        # transforms.RandomCrop(32, padding=4), #随机裁剪
+        # transforms.RandomHorizontalFlip(), # 翻转图片
+        transforms.ToTensor()
+    ])
+    train_dataset = MyDataset(datapath, True, data_transforms)
+    train_loader = DataLoader(train_dataset, 4)
+    test_dataset = MyDataset(datapath, False, data_transforms)
+    test_loader = DataLoader(test_dataset, 1)
+    return (train_loader, test_loader)
+
+
+def train(trainLoader, model, lossFunction, optimizer, device):
+    model.train()
+    tloss, totalBatch = 0, 0
+    tiou, totalSize = 0.0, 0
+    for batch, (data, label) in enumerate(trainLoader):
+        totalBatch += 1
+        # convert
+        data, label = data.to(device), label.to(device)
+        label = label.to(torch.int64).squeeze(dim=1)
+        # calculate
+        pred = model(data)
+        loss = lossFunction(pred, label)
+        # print(pred.shape, label.shape, loss)
+        # optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        # stats
+        tloss += loss.item()
+        totalSize += label.shape[0]
+        tiou += calMIOU(pred, label)
+        # print
+        if (batch+1) % 10 == 0:
+            nowTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            print(f'[{nowTime}]    Batch: {batch+1:>4}, Loss:{loss.item():>7.6f}, mIOU:{tiou/totalSize:>6.4f}')
+    return (tloss/totalBatch, tiou/totalSize)
+
+
+def test(testLoader, model, lossFunction, device):
+    model.eval()
+    tloss, totalBatch = 0, 0
+    tiou = 0.0
+    totalCount = len(testLoader)
+    with torch.no_grad():
+        for batch, (data, label) in enumerate(testLoader):
+            totalBatch += 1
+            # convert
+            data, label = data.to(device), label.to(device)
+            label = label.to(torch.int64).squeeze(dim=1)
+            # calculate
+            pred = model(data)
+            loss = lossFunction(pred, label)
+            # add
+            tloss += loss.item()
+            tiou += calMIOU(pred, label)
+    return (tloss/totalBatch, tiou/totalCount)
+
+
+def main():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f'Using {device} device')
+
+    trainLoader, testLoader = loadData()
+
+    model = DeepLab(backbone='resnet', output_stride=16, num_classes=9).to(device)
+    lossFunction = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
+
+    print("Begin Training")
+    for epoch in range(64):
+        print(f"Epoch:{epoch+1:>3}, learning rate = {0.1}")
+        # if (epoch==31):
+        #     lr = lr /10
+        #     optimizer.param_groups[0]["lr"]=lr
+        # if (epoch==47):
+        #     lr = lr /10
+        #     optimizer.param_groups[0]["lr"]=lr 
+
+        trainLoss, trainAcc = train(trainLoader, model, lossFunction, optimizer, device)
+        testLoss, testAcc = test(testLoader, model, lossFunction, device)
+        with open('./result.txt', 'a') as f:
+            nowTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            f.write(f'[{nowTime}] Epoch{epoch+1:>3d}\n')
+            f.write(f'    Train: Loss {trainLoss:>7.6f}, mIOU {trainAcc:>6.4f}\n')
+            f.write(f'    Test:  Loss {testLoss :>7.6f}, mIOU {testAcc :>6.4f}\n')
+        torch.save(model.state_dict(), './saves/model.pth')
+    print("Done")
+
+
+if __name__ == '__main__':
+    main()
