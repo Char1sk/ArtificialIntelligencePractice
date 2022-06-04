0434fdf2e73cac591fd129815bce76fcbdbfa2fd
Charisk <2553036255@qq.com>
2022-06-04 10:37:56 +0800

    MIOU not ignore unknown

--- Program2-Segmentation/utils.py
+++ Program2-Segmentation/utils.py
@@ -1,24 +1,18 @@
 import numpy as np
 
 
-def calMIOU(pred, label):
+def calMIOU(pred, label, ignore_unknown=False):
     nc = pred.shape[1]
     _, index = pred.max(dim=1)
-    # print(index.shape, label.shape)
     index, label = index.cpu().numpy(), label.cpu().numpy()
     miou = 0.0
     for i, l in zip(index, label):
-        # print(i.shape, l.shape)
         hist = np.zeros((nc-1, nc-1), dtype=int)
         for ri, rl in zip(i, l):
-            # print(nc*rl.astype(int)+ri)
             hist += np.bincount(nc*rl.astype(int)+ri, minlength=nc**2).reshape(nc, nc)[:-1,:-1]
-            # print(hist.shape)
-        # print('hist:', hist)
-        # print(np.diag(hist))
-        # print((hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist)))
+        if ignore_unknown:
+            hist = hist[:-1,:-1]
         miou += (np.diag(hist) / (hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist) + 1) ).mean()
-        # print('miou:', miou)
     return miou
 
 def calPA(pred, label):
